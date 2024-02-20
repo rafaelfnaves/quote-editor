4b58cbb5a314202c4d9005efc69083a330bfd80c
Rafael Naves <r.naves21@gmail.com>
2024-02-20 16:15:53 -0300

    Create line_item

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'line_items/new'
+  get 'line_items/create'
   root to: "pages#home"
   
   get 'pages/home'
   devise_for :users
 
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
   end
 end

f6b28f71a5765d04c065475e474af3761c38e407
Rafael Naves <r.naves21@gmail.com>
2024-02-20 10:44:19 -0300

    Turbo Stream with Security

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "pages#home"
+  
+  get 'pages/home'
+  devise_for :users
+  
   resources :quotes
 end
